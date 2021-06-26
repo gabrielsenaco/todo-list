e28095ea72b5ee42a8d15671676b5525f3cf45c3
Gabriel Sena <gabesenacom@gmail.com>
2021-06-26 19:34:18 -0300

    Add main.js production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  //development
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
